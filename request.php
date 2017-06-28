73fc3010088dd2674f85a02493057602873f88cb
Sierra Katow <skatow@college.harvard.edu>
2017-06-28 15:19:06 -0700

    Allow search across multiple colors

--- request.php
+++ request.php
@@ -6,7 +6,7 @@ $max_cluster_count = 4;
 // COLOR CLUSTERS
 $colors = array();
 
-for($i=1;$i<$max_cluster_count;$i++){
+for($i=1;$i<=$max_cluster_count;$i++){
     if($_GET['color'.$i] !== null) {
         $colors[$i] = explode(',', $_GET['color'.$i]);
         // get integer from color cluster GET var
@@ -21,36 +21,31 @@ $cluster_count = sizeof($colors);
 $limit = ($_GET['limit'] === null) ? null : intval($_GET['limit']); // DEFINE LIMIT
 $offset = ($_GET['offset'] === null) ? null : intval($_GET['offset']); // DEFINE OFFSET
 
-$select_str = 'SELECT items.id, items.link, items.img_url, items.price, items.category_id, items.serial_number, 
+$select_str = 'SELECT items_colors.item_id, items.link, items.img_url, items.price, items.category_id, items.serial_number, items_colors.rank,
     CONCAT(items.R1, \', \', items.G1, \', \', items.B1, \', \', items.P1) as color1, 
     CONCAT(items.R2, \', \', items.G2, \', \', items.B2, \', \', items.P2) as color2, 
     CONCAT(items.R3, \', \', items.G3, \', \', items.B3, \', \', items.P3) as color3, 
     CONCAT(items.R4, \', \', items.G4, \', \', items.B4, \', \', items.P4) as color4';
 
 // ADD COLOR CLUSTERS TO QRY
-for($i=1;$i<$max_cluster_count;$i++){
+for($i=1;$i<=$max_cluster_count;$i++){
     if(array_key_exists($i, $colors)){
-        $select_str .= ', SQRT(POW(R'.$i.'-:R'.$i.', 2)+POW(G'.$i.'-:G'.$i.',2)+POW(B'.$i.'-:B'.$i.',2)) as distance'.$i;
+        $select_str .= ', SQRT(POW(items_colors.R-:R'.$i.', 2)+POW(items_colors.G-:G'.$i.',2)+POW(items_colors.B-:B'.$i.',2)) as distance'.$i;
     }
-    // if(array_key_exists($i, $colors)){
-    //     $select_str .= '`R'.$i.'` = :R'.$i.' AND `G'.$i.'` = :G'.$i.' AND `B'.$i.'` = :B'.$i;
-    // }
-    // if($colors[$i])
-    // if($i < $max_cluster_count-1) $select_str .= ' AND ';
 }
 
-$select_str .= ' FROM items';
+$select_str .= ' FROM items_colors INNER JOIN items ON items.id = items_colors.item_id';
 
 // ORDER WITH COLOR CLUSTERS
 if($cluster_count == 1){
-    $select_str .= ' ORDER BY distance1, P1 DESC, id'; 
+    $select_str .= ' ORDER BY distance1, items_colors.P DESC, items_colors.item_id'; 
 }elseif($cluster_count > 1){
     $select_str .= ' ORDER BY SQRT(';
-    for($i=1;$i<$max_cluster_count;$i++){
+    for($i=1;$i<$cluster_count;$i++){
         $select_str .= 'POW(distance'.$i.', 2)';
-        if($i < $max_cluster_count-1) $select_str .= ' + ';
+        if($i < $cluster_count-1) $select_str .= ' + ';
     }
-    $select_str .= '), id';
+    $select_str .= '), items_colors.item_id';
 }
 
 
@@ -62,7 +57,7 @@ if($offset !== null) $select_str .= ' OFFSET :offset'; // ADD OFFSET TO QRY
 $select = $pdo->prepare($select_str);
 
 // BIND COLOR PARAMS
-for($i=1;$i<$max_cluster_count;$i++){
+for($i=1;$i<=$max_cluster_count;$i++){
     if(array_key_exists($i, $colors)){
         $select->bindParam(':R'.$i, $colors[$i][0], PDO::PARAM_INT);
         $select->bindParam(':G'.$i, $colors[$i][1], PDO::PARAM_INT);
