3376d682ca46ed1f8c796d9cff4a614120284ade
Sierra Katow <skatow@college.harvard.edu>
2017-06-27 21:25:28 -0700

    Pull from copy table

--- request.php
+++ request.php
@@ -11,11 +11,16 @@ if(isset($_GET['color1'])) {
     exit();
 }
 
-if(isset($_GET['limit']) && is_int($_GET['limit'])){
-    $limit = $_GET['limit'];
+if(isset($_GET['limit'])){
+    $limit = intval($_GET['limit']);
 }
 
-$select_str = 'SELECT * FROM items 
+$select_str = 'SELECT items.id, items.link, items.img_url, items.price, items.category_id, items.serial_number, 
+    CONCAT(items.R1, \', \', items.G1, \', \', items.B1, \', \', items.P1) as color1, 
+    CONCAT(items.R2, \', \', items.G2, \', \', items.B2, \', \', items.P2) as color2, 
+    CONCAT(items.R3, \', \', items.G3, \', \', items.B3, \', \', items.P3) as color3, 
+    CONCAT(items.R4, \', \', items.G4, \', \', items.B4, \', \', items.P4) as color4
+    FROM items_copy 
     WHERE `R1` = :R1 
     AND `G1` = :G1 
     AND `B1` = :B1';
@@ -28,13 +33,13 @@ $select->bindParam(':R1', $color1[0], PDO::PARAM_INT);
 $select->bindParam(':G1', $color1[1], PDO::PARAM_INT);
 $select->bindParam(':B1', $color1[2], PDO::PARAM_INT);
 
-if(isset($limit)) $select = $pdo->prepare($select_str); // Add limit value
+if(isset($limit)) $select->bindParam(':lim', $limit, PDO::PARAM_INT); // Add limit value
 
 try{
     $select->execute();
     $result = $select->fetchAll(\PDO::FETCH_OBJ);
     foreach($result as $row){
-        print_r($row);
+        print(json_encode($row)."\n");
     }
 }catch(\PDOException $ex){
     print($ex->getMessage());
