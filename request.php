5d8e630696cd8ef21c0a7b021865f10d54b90073
Sierra Katow <skatow@college.harvard.edu>
2017-06-28 20:19:10 -0700

    Added: Percentage (for 1st color) + category search

--- request.php
+++ request.php
@@ -2,50 +2,111 @@
 include('./connection.php');
 
 $max_cluster_count = 4;
+$threshold = 80;
 
-// COLOR CLUSTERS
-$colors = array();
+// CHECK PARAMETERS
+if($_GET['color1'] !== null) {
+    $color1 = explode(',', $_GET['color1']);
+    // get integer from color cluster GET var
+    foreach($color1 as $j => $c){
+        $color1[$j] = intval($c);
+    }
 
-for($i=1;$i<=$max_cluster_count;$i++){
-    if($_GET['color'.$i] !== null) {
-        $colors[$i] = explode(',', $_GET['color'.$i]);
+    if($_GET['colors2'] !== null){
+        $color2 = explode(',', $_GET['color2']);
         // get integer from color cluster GET var
-        foreach($colors[$i] as $j => $c){
-            $colors[$i][$j] = intval($c);
+        foreach($color2 as $j => $c){
+            $color2[$j] = intval($c);
         }
     }
+    // percentage filters only present if color1 exists
+    $pmin = ($_GET['pmin'] === null) ? null : intval($_GET['pmin']);
+    $pmax = ($_GET['pmax'] === null) ? null : intval($_GET['pmax']);
+    
+}elseif($_GET['colorscheme'] !== null){
+    $colorscheme = intval($_GET['colorscheme']);
 }
 
-$cluster_count = sizeof($colors);
+$category = ($_GET['category'] === null) ? null : intval($_GET['category']);
+$pattern = ($_GET['pattern'] === null) ? null : intval($_GET['pattern']);
 
 $limit = ($_GET['limit'] === null) ? null : intval($_GET['limit']); // DEFINE LIMIT
 $offset = ($_GET['offset'] === null) ? null : intval($_GET['offset']); // DEFINE OFFSET
 
-$select_str = 'SELECT items_colors.item_id, items.link, items.img_url, items.price, items.category_id, items.serial_number,
-    CONCAT(items.R1, \', \', items.G1, \', \', items.B1, \', \', items.P1) as color1, 
-    CONCAT(items.R2, \', \', items.G2, \', \', items.B2, \', \', items.P2) as color2, 
-    CONCAT(items.R3, \', \', items.G3, \', \', items.B3, \', \', items.P3) as color3, 
-    CONCAT(items.R4, \', \', items.G4, \', \', items.B4, \', \', items.P4) as color4';
 
-// ADD COLOR CLUSTERS TO QRY
-for($i=1;$i<=$max_cluster_count;$i++){
-    if(array_key_exists($i, $colors)){
-        $select_str .= ', MIN(SQRT(POW(items_colors.R-:R'.$i.', 2) + POW(items_colors.G-:G'.$i.', 2) + POW(items_colors.B-:B'.$i.', 2))) AS `distance'.$i.'`';
+if($colors1 !== null){
+    $select_str = 'SELECT ic.item_id, items.link, items.img_url, items.price, items.category_id, items.serial_number,
+        items.R1, items.G1, items.B1, items.P1,
+        items.R2, items.G2, items.B2, items.P2,
+        items.R3, items.G3, items.B3, items.P3,
+        items.R4, items.G4, items.B4, items.P4';
+}else{
+    $select_str = 'SELECT id as `item_id`, link, img_url, price, category_id, serial_number, 
+        R1, G1, B1, P1, 
+        R2, G2, B2, P2, 
+        R3, G3, B3, P3, 
+        R4, G4, B4, P4
+    FROM items';
+}
+
+// SINGLE COLOR QUERY
+if($color1 !== null){
+    if($color2 === null) {
+        // Single Color
+        $select_str .= ', SQRT(POW(ic.R-:R1, 2) + 
+                                    POW(ic.G-:G1, 2) + 
+                                    POW(ic.B-:B1, 2))
+                        AS `distance`';
+    }else{
+        // Double Color
+        $select_str .= ', SQRT(';
     }
 }
 
-$select_str .= ' FROM items_colors INNER JOIN items ON items.id = items_colors.item_id';
+// FROM TABLE
+if($color1 !== null) {
+    if($color2 === null) {
+        $select_str .= ' FROM items_colors ic';
+    } else {
+        $select_str .= ' FROM items_colors2 ic';
+    }
+}
+
+// JOIN ON ITEMS DETAILS
+if($color1 !== null) $select_str .= ' INNER JOIN items ON items.id = ic.item_id';
+
+// ADD WHERE
+if($category !== null || $pmin !== null || $pmax !== null || $colorscheme !== null || $pattern !== null){
+    $select_str .= ' WHERE';
+}
+
+
+
+// FILTER
+if($category !== null) {
+    if($color1 === null) $select_str .= ' category_id = :category';
+    else $select_str .= ' items.category_id = :category';
+}
+
+if($pmin !== null){
+    $select_str .= ' ic.P >= :pmin';
+}
+
+if($pmax !== null){
+    $select_str .= ' ic.P <= :pmax';
+}
 
 // ORDER WITH COLOR CLUSTERS
-if($cluster_count == 1){
-    $select_str .= ' GROUP BY items_colors.item_id ORDER BY distance1, items_colors.item_id'; 
-}elseif($cluster_count > 1){
-    // $select_str .= ' ORDER BY SQRT(';
-    // for($i=1;$i<$cluster_count;$i++){
-    //     $select_str .= 'POW(distance'.$i.', 2)';
-    //     if($i < $cluster_count-1) $select_str .= ' + ';
-    // }
-    // $select_str .= '), items_colors.item_id';
+if($color1 !== null){
+    if($color2 === null) { 
+    // Single Color
+        $select_str .= ' HAVING distance < \''.$threshold.'\' ORDER BY distance, ic.P DESC, ic.item_id'; 
+    }else{
+    // Double Color
+
+    }
+}else{
+    $select_str .= ' ORDER BY item_id';
 }
 
 
@@ -53,18 +114,30 @@ if($cluster_count == 1){
 if($limit !== null) $select_str .= ' LIMIT :lim'; // ADD LIMIT TO QRY
 if($offset !== null) $select_str .= ' OFFSET :offset'; // ADD OFFSET TO QRY
 
+// print $select_str;
 
 $select = $pdo->prepare($select_str);
 
 // BIND COLOR PARAMS
-for($i=1;$i<=$max_cluster_count;$i++){
-    if(array_key_exists($i, $colors)){
-        $select->bindParam(':R'.$i, $colors[$i][0], PDO::PARAM_INT);
-        $select->bindParam(':G'.$i, $colors[$i][1], PDO::PARAM_INT);
-        $select->bindParam(':B'.$i, $colors[$i][2], PDO::PARAM_INT);
+if($color1 !== null){
+    // Single Color
+    $select->bindParam(':R1', $color1[0], PDO::PARAM_INT);
+    $select->bindParam(':G1'.$i, $color1[1], PDO::PARAM_INT);
+    $select->bindParam(':B1'.$i, $color1[2], PDO::PARAM_INT);
+
+    // Double Color
+    if($color2 !== null){
+        $select->bindParam(':R2', $color2[0], PDO::PARAM_INT);
+        $select->bindParam(':G2'.$i, $color2[1], PDO::PARAM_INT);
+        $select->bindParam(':B2'.$i, $color2[2], PDO::PARAM_INT);
     }
+
+    // Percentage
+    if($pmin !== null) $select->bindParam(':pmin', $pmin);
+    if($pmax !== null) $select->bindParam(':pmax', $pmax);
 }
 
+if($category !== null) $select->bindParam(':category', $category, PDO::PARAM_INT); // BIND CATEGORY ID
 if($limit !== null) $select->bindParam(':lim', $limit, PDO::PARAM_INT); // BIND LIMIT VAL
 if($offset !== null) $select->bindParam(':offset', $offset, PDO::PARAM_INT); // BIND OFFSET VAL
 
@@ -82,7 +155,19 @@ if($limit !== null) {
 // EXECUTION + DATA RETRIEVAL
 try{
     $select->execute();
-    $result = $select->fetchAll(\PDO::FETCH_OBJ);
+    $result = $select->fetchAll(PDO::FETCH_ASSOC);
+
+    // format color clusters
+    foreach($result as $k => $row){
+        $result[$k]['colors'] = array();
+        for($i=1;$i<=$max_cluster_count;$i++){
+            array_push($result[$k]['colors'], array($row['R'.$i], $row['G'.$i], $row['B'.$i], $row['P'.$i]));
+            unset($result[$k]['R'.$i]);
+            unset($result[$k]['G'.$i]);
+            unset($result[$k]['B'.$i]);
+            unset($result[$k]['P'.$i]);
+        }
+    }
 
     $offset = ($offset === null) ? 0 : $offset;
 
