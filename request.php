7f74dc0f758e4d4386e2e7244f2382bb46e4846c
Blatt <blatt@Blatts-iMac.attlocal.net>
2017-07-08 23:13:54 -0700

    next

--- request.php
+++ request.php
@@ -131,7 +131,7 @@ if($color2 === null){
     
     $top_green=min(':G1'+$color_threshold,255);
     $bottom_green=max(0,':G1'-$color_threshold);
-    $select_str .= ' ic.G <= '.$top_green.' AND ';
+    $select_str .= ' ic.G <= (:G1+'.$top_green.') AND ';
   //  $select_str .= ' ic.G >= '.$bottom_green.' AND ';
     
   //  $top_blue=min(':B1'+$color_threshold,255);
