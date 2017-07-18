6914455a9015e7348ea64c7533e9f35b1ddf4b7a
Blatt <blatt@Blatts-iMac.attlocal.net>
2017-07-18 14:07:41 -0700

    tsttwo

--- write.php
+++ write.php
@@ -3,7 +3,11 @@ include('./connect_record.php');
 
 
 
-$select_str="INSERT INTO MasterRecords (USERID,CurTime,NumColors,Scheme,Pattern,R1,G1,B1,R2,G2,B2) VALUES (16,1000,2,0,0,100,200,100,300,300,300)"
+
+$select_str="INSERT INTO MasterRecords (UserID) VALUES (101)";
+
+$
+
 $select = $pdo->prepare($select_str);
 $select->execute();
 
