b7fc76265e84f8b48071448b71bde0bb5f04ed79
Blatt <blatt@Blatts-iMac.attlocal.net>
2017-07-18 14:30:25 -0700

    NewWrite3

--- writeit.php
+++ writeit.php
@@ -3,10 +3,20 @@ include('./connection.php');
 
 
 $UID = $_GET['UID'];
+$CTime = $_GET['CTime'];
+$NumC = $_GET['NumC'];
+$SCEM = $_GET['SCEM'];
+$Pat = $_GET['Pat'];
+$R1 = $_GET['R1'];
+$G1 = $_GET['G2'];
+$B1 = $_GET['B2'];
+$R2 = $_GET['R2'];
+$G2 = $_GET['G2'];
+$B2 = $_GET['B2'];
 
 
 
-$select_str='INSERT INTO MasterRecords (UserID) VALUES ('.$UID.')';
+$select_str='INSERT INTO MasterRecords (UserID,CurTime,NumColors,Scheme,Pattern,R1,G1,B1,R2,G2,B2) VALUES ('.$UID.','.$CTime.','.$NumC.','.$SCEM.','.$Pat.','.$R1.','.$G1.','.$B1.','.$R2.','.$G2.','.$B2.')';
 
 
 
