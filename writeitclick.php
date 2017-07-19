80aed9c6a6d8c2fa062385425150198598efcee0
Blatt <blatt@Blatts-iMac.attlocal.net>
2017-07-18 17:55:09 -0700

    FCom

--- writeitclick.php
+++ writeitclick.php
@@ -18,7 +18,7 @@ $CAT = $_GET['Cat'];
 
 
 
-$select_str='INSERT INTO MasterClicks (UserID,CurTime,NumColors,Scheme,Pattern,R1,G1,B1,R2,G2,B2,ITEMLINK,CATEGORY) VALUES ("'.$UID.'",'.$CTime.','.$NumC.','.$SCEM.','.$Pat.','.$R1.','.$G1.','.$B1.','.$R2.','.$G2.','.$B2.','.$IL.','.$CAT.')';
+$select_str='INSERT INTO MasterClicks (UserID,CurTime,NumColors,Scheme,Pattern,R1,G1,B1,R2,G2,B2,ITEMLINK,CATEGORY) VALUES ("'.$UID.'",'.$CTime.','.$NumC.','.$SCEM.','.$Pat.','.$R1.','.$G1.','.$B1.','.$R2.','.$G2.','.$B2.',"'.$IL.'","'.$CAT.'")';
 
 echo $select_str;
 
