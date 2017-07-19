3875449b0d03e837e26cd4de7ca802eb0226559e
Blatt <blatt@Blatts-iMac.attlocal.net>
2017-07-18 17:51:05 -0700

    FCom

--- writeitclick.php
+++ writeitclick.php
@@ -20,7 +20,7 @@ $CAT = $_GET['Cat'];
 
 $select_str='INSERT INTO MasterClicks (UserID,CurTime,NumColors,Scheme,Pattern,R1,G1,B1,R2,G2,B2,ITEMLINK,CATEGORY) VALUES ("'.$UID.'",'.$CTime.','.$NumC.','.$SCEM.','.$Pat.','.$R1.','.$G1.','.$B1.','.$R2.','.$G2.','.$B2.','.$IL.','.$CAT.')';
 
-
+echo select_str
 
 $select = $pdo->prepare($select_str);
 $select->execute();
